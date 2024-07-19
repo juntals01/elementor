070037a95ffca3909cab35ca45593b4d11075d36
jun <norbertoqjr@gmail.com>
2024-07-19 23:05:08 +0800

    elemetor initial

---
+++ html/wp-content/plugins/templates-patterns-collection/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/templates-patterns-collection',
+        'pretty_version' => '1.2.10',
+        'version' => '1.2.10.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/templates-patterns-collection' => array(
+            'pretty_version' => '1.2.10',
+            'version' => '1.2.10.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.25',
+            'version' => '3.3.25.0',
+            'reference' => '29b7c81f8ccd039f49d62ef7427a4cc06369becc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
